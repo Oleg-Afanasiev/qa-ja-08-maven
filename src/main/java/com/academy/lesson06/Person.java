ed46244f2e8ac2590a32ec3ca113b631938c78cd
Oleg <oleg.kh81@gmail.com>
2019-11-22 21:08:12 +0200

    init maven project

---
+++ src/main/java/com/academy/lesson06/Person.java
@@ -0,0 +1,71 @@
+package com.academy.lesson06;
+
+import java.util.Comparator;
+
+public class Person implements Comparable<Person> {
+    protected long id;
+    protected String name;
+    protected Gender gender;
+
+    public Person() {
+    }
+
+    public Person(long id, String name, Gender gender) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public void doCommonJob(String typeOfJob) {
+        System.out.println("Я делаю " + typeOfJob);
+    }
+
+    public void about() {
+        System.out.println("Я человек");
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        if (this.id < other.id)
+            return -1;
+
+        if (this.id > other.id)
+            return 1;
+
+        return 0;
+    }
+}
