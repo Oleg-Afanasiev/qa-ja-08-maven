7554d971ff2ffe08588ba475121d85efbd45c5ea
Oleg <oleg.kh81@gmail.com>
2019-12-06 21:10:28 +0200

    test init

---
+++ src/test/java/com/academy/automationpractice/LoginTests.java
@@ -0,0 +1,46 @@
+package com.academy.automationpractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+    private WebDriver driver;
+    private String baseUrl;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws IOException {
+        Properties properties = new Properties();
+        // из директории <root>/src/main/java/resources
+        properties.load(this.getClass().getClassLoader().getResourceAsStream("common.properties"));
+        System.setProperty("webdriver.chrome.driver", properties.getProperty("chrome.driver"));
+        driver = new ChromeDriver();
+        baseUrl = properties.getProperty("baseUrl");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testIncorrectLogin() {
+        driver.get(baseUrl);
+        driver.findElement(By.linkText("Sign in")).click();
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).clear();
+        driver.findElement(By.id("email")).sendKeys("qwerty");
+        driver.findElement(By.id("passwd")).click();
+        driver.findElement(By.id("passwd")).clear();
+        driver.findElement(By.id("passwd")).sendKeys("1234");
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Forgot your password?'])[1]/following::span[1]")).click();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+}
